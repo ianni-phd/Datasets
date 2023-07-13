655f2172f2dec0ebd06377a3f22742267c9a5468
ianniphd <10ianni@gmail.com>
2023-07-13 21:51:48 +0200

    Add files via upload

---
+++ Immobile/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+import numpy as np
+
+
+def analysis_1():
+    df = pd.read_csv('ciro_immobile.csv', delimiter='\t')
+    df
+
+def main():
+    analysis_1()
+
+if __name__ == "__main__":
+    main()
